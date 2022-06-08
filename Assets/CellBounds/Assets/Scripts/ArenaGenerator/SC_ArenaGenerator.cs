e62dff8cc9368672119c75773384e488c8969ffc
Daniel <daniel.hramkov@gmail.com>
2022-06-08 10:31:09 +0500

    Update ArenaGenerator

--- Assets/CellBounds/Assets/Scripts/ArenaGenerator/SC_ArenaGenerator.cs
+++ Assets/CellBounds/Assets/Scripts/ArenaGenerator/SC_ArenaGenerator.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,44 +7,36 @@ using UnityEngine;
 public class SC_ArenaGenerator : MonoBehaviour
 {
     public Texture2D tex;
-    public GameObject CT_Ground;
-    public GameObject CT_Tree;
-    public GameObject CT_Water;
+
+    public List<GameObject> CellTiles = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
-    {
-        Color Black = new Color(0,0,0);
-        Color Red = new Color(1,0,0);
-        Color Green = new Color(0,1,0);
-        Color Blue = new Color(0,0,1);
-        
+    {        
         int Row = tex.width;
         for (var i = 0; i < Row; i++)
         {
             for (var f = 0; f < Row; f++)
             {
-                Color PixelColor = tex.GetPixel(i,f);
+                double PixelColor = Math.Round(tex.GetPixel(i,f)[0], 2);
                 Vector3 position = new Vector3(i,0,f);
                 
-                if(PixelColor == Red)
+                
+                switch (PixelColor)
                 {
-                    Debug.Log(1);
-                    Instantiate(CT_Ground, position, Quaternion.identity);
+                    case 1:
+                        Instantiate(CellTiles[0], position, Quaternion.identity);
+                        break;
+                    case 0.95:
+                        Instantiate(CellTiles[1], position, Quaternion.identity);
+                        break;
+                    case 0.9:
+                        Instantiate(CellTiles[2], position, Quaternion.identity);
+                        break;
+                    default:
+                        break;
                 }
-                else if (PixelColor == Green)
-                {
-                    Debug.Log(2);
-                    Instantiate(CT_Tree, position, Quaternion.identity);
-                }
-                else if (PixelColor == Blue) {
-                    Debug.Log(3);
-                    Instantiate(CT_Water, position, Quaternion.identity);
-                }
-                else {
-                    Debug.Log(4);
-                }
-               
+                           
             }
         }
     }

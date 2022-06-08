9e2e9381d2d94795b7e4e4a9ee21c687cb2200ce
Daniel <daniel.hramkov@gmail.com>
2022-06-08 11:09:05 +0500

    Create CameraController

--- Assets/CellBounds/Assets/Scripts/Player/CameraController.cs
+++ Assets/CellBounds/Assets/Scripts/Player/CameraController.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speedMovement = 20f;
+    public float scrollSpeed = 20f;
+
+    public float arenaSize = 128;
+
+    float cameraRotation = 60;
+
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position;
         
+        if (Input.GetAxis("Fire2") == 1)
+        {
+            pos.x = pos.x + Input.GetAxis("Mouse X") * -1;
+            pos.z = pos.z + Input.GetAxis("Mouse Y") * -1;
+        }
+
+        pos.y += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime * -1;
+        Camera.main.fieldOfView += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime * -1;
+        cameraRotation += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime * -1;
+
+
+        pos.y = Mathf.Clamp(pos.y, 10f, 60.1f);
+        pos.x = Mathf.Clamp(pos.x, 0f, arenaSize);
+        pos.z = Mathf.Clamp(pos.z, 0f, arenaSize);
+        cameraRotation = Mathf.Clamp(cameraRotation, 21f, 80f);
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 40f, 80f);
+
+        transform.eulerAngles = new Vector3(cameraRotation, 0, 0);
+        
+        transform.position = pos;
     }
 }
